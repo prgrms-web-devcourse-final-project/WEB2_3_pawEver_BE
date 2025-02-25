e62edda6bda3b9c736bb31c19d063bce36e5b353
jaehyunpyun <pyunjaehyun@gmail.com>
2025-02-25 16:07:26 +0900

    fix: buildgradle의 sourceset 제거 및 SpringBootTest에 ActiveProfiles 어노테이션으로 test 지정

--- src/test/java/com/pawever/server/domain/cicd/service/RedisServiceTest.java
+++ src/test/java/com/pawever/server/domain/cicd/service/RedisServiceTest.java
@@ -3,9 +3,12 @@ package com.pawever.server.domain.cicd.service;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class RedisServiceTest {
     // Mock 객체 없이 실제 Redis 테스트
 
