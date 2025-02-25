338f9762f2731ca3d8d9929c24e1b32f980a48ae
jaehyunpyun <pyunjaehyun@gmail.com>
2025-02-25 17:18:42 +0900

    fix: @SpringbootTest에 @activeProfiles("test") 추가하여 재실행

--- src/test/java/com/pawever/server/domain/cicd/service/RedisServiceTest.java
+++ src/test/java/com/pawever/server/domain/cicd/service/RedisServiceTest.java
@@ -3,13 +3,14 @@ package com.pawever.server.domain.cicd.service;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 
 @SpringBootTest
-// @ActiveProfiles("test")
+@ActiveProfiles("test")
 public class RedisServiceTest {
 
     @Autowired
