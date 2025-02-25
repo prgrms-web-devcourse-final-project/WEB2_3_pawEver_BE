add97c7d582dc4306bf4c10474350c68d31dd25c
jaehyunpyun <pyunjaehyun@gmail.com>
2025-02-25 15:19:46 +0900

    feat: 같은 상황에서 에러 발생하는지 확인하기 위해 RedisServiceTest 주석 해제

--- src/test/java/com/pawever/server/domain/cicd/service/RedisServiceTest.java
+++ src/test/java/com/pawever/server/domain/cicd/service/RedisServiceTest.java
@@ -9,21 +9,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class RedisServiceTest {
     // Mock 객체 없이 실제 Redis 테스트
 
-//    @Autowired
-//    private RedisService redisService;
-//
-//    @Test
-//    public void testSaveAndGetValue() {
-//        // given
-//        String key = "testKey";
-//        String value = "testValue";
-//
-//        // when
-//        redisService.saveValue(key, value);
-//        String retrievedValue = redisService.getValue(key);
-//
-//        // then
-//        assertThat(retrievedValue).isEqualTo(value);
-//        System.out.println("Redis Test without Mock success");
-//    }
+    @Autowired
+    private RedisService redisService;
+
+    @Test
+    public void testSaveAndGetValue() {
+        // given
+        String key = "testKey";
+        String value = "testValue";
+
+        // when
+        redisService.saveValue(key, value);
+        String retrievedValue = redisService.getValue(key);
+
+        // then
+        assertThat(retrievedValue).isEqualTo(value);
+        System.out.println("Redis Test without Mock success");
+    }
 }
